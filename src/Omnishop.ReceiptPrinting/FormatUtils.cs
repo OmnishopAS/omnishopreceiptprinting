688edc47338ecb738e3d27b5d1dbdf988e69eb48
andulv <anders@ulveseth.net>
2017-11-19 02:08:47 +0100

    Formatmetode for string output

--- src/Omnishop.ReceiptPrinting/FormatUtils.cs
+++ src/Omnishop.ReceiptPrinting/FormatUtils.cs
@@ -6,7 +6,7 @@ namespace Omnishop.ReceiptPrinting
 {
     public static class FormatUtils
     {
-        public static string CutOrPadLeft(string value, int totalLength)
+        public static string CutOrPadLeft(this string value, int totalLength)
         {
             if (string.IsNullOrEmpty(value))
                 return new string(' ', totalLength);
@@ -15,6 +15,17 @@ namespace Omnishop.ReceiptPrinting
                 return value.Substring(0, totalLength);
 
             return value.PadLeft(totalLength);
+        }
+
+        public static string CutOrPadRight(this string value, int totalLength)
+        {
+            if (string.IsNullOrEmpty(value))
+                return new string(' ', totalLength);
+
+            if (value.Length >= totalLength)
+                return value.Substring(0, totalLength);
+
+            return value.PadRight(totalLength);
         }
     }
 }
