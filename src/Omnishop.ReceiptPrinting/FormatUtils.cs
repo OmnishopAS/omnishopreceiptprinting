9e4cd5bdca660d36089e9267c82a2fea517ecfeb
andulv <anders@ulveseth.net>
2018-02-11 22:22:38 +0100

    CutOrPadRight

--- src/Omnishop.ReceiptPrinting/FormatUtils.cs
+++ src/Omnishop.ReceiptPrinting/FormatUtils.cs
@@ -16,5 +16,17 @@ namespace Omnishop.ReceiptPrinting
 
             return value.PadLeft(totalLength);
         }
+
+        public static string CutOrPadRight(string value, int totalLength)
+        {
+            if (value == null)
+                return new string(' ', totalLength);
+
+            if (value.Length >= totalLength)
+                return value.Substring(0, totalLength);
+
+            return value.PadRight(totalLength, ' ');
+        }
+
     }
 }
