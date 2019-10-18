c388f56f13c7564b9c4fbce5034ebd121e056487
Anders Ulveseth <anders@definit.no>
2019-10-19 01:32:58 +0200

    Added method for splitting string

--- src/Omnishop.ReceiptPrinting/FormatUtils.cs
+++ src/Omnishop.ReceiptPrinting/FormatUtils.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Omnishop.ReceiptPrinting
@@ -26,6 +27,29 @@ namespace Omnishop.ReceiptPrinting
                 return value.Substring(0, totalLength);
 
             return value.PadRight(totalLength, ' ');
+        }
+
+        public static IEnumerable<string> Split(string str, int chunkSize)
+        {
+            var retValue = new List<string>();
+            if (String.IsNullOrEmpty(str))
+            {
+                retValue.Add(String.Empty);
+            }
+            else
+            {
+                int index = 0;
+                while (index < str.Length)
+                {
+                    var subStringLength = Math.Min(chunkSize, str.Length - index);
+                    retValue.Add(str.Substring(0, subStringLength));
+                    index += subStringLength;
+                }
+
+                return retValue.Select(x => CutOrPadRight(x, chunkSize));
+            }
+            return retValue;
+
         }
 
     }
